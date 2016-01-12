9f5a1f2c137191f3632066ececb1843a594f79cd
mwraich <mwraich28@gmail.com>
2016-01-12 16:04:15 -0500

    Completed and optimized exercise 4

--- exercise4.rb
+++ exercise4.rb
@@ -1,12 +1,13 @@
 one_to_one_hundred = (1..100)
 
 one_to_one_hundred.each do |num|
-when (num % 3 == 0)
-  puts "Bit"
-when (num % 5 == 0)
-  puts "Maker"
-when (num % 5 == 0 and num % 3 ==0)
-  puts "BitMaker"
-else
-  puts num
+  if ( num % 3 == 0 and num % 5 == 0)
+    puts "BitMakers"
+  elsif (num % 5 == 0)
+    puts "Maker"
+  elsif (num % 3 ==0)
+    puts "Bit"
+  else
+    puts num
+  end
 end
