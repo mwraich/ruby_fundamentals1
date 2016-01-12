eed83f231c86e541e001a3af59403e0695aa9d31
mwraich <mwraich28@gmail.com>
2016-01-12 15:32:32 -0500

    Switched to When statements

--- exercise4.rb
+++ exercise4.rb
@@ -1,10 +1,12 @@
 one_to_one_hundred = (1..100)
 
 one_to_one_hundred.each do |num|
-if num % 3 = 0
+when (num % 3 == 0)
   puts "Bit"
-elsif num % 5 = 0
+when (num % 5 == 0)
   puts "Maker"
+when (num % 5 == 0 and num % 3 ==0)
+  puts "BitMaker"
 else
   puts num
 end
